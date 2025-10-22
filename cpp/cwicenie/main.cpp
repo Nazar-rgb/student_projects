3f781338956d1d23b6936d82606fede6fa1c25ab
Nazar-rgb <nazarbudinkevic5@gmail.com>
2025-10-22 11:10:06 +0000

    studentmachine send

--- cpp/cwicenie/main.cpp
+++ cpp/cwicenie/main.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main(){
-	cout<<"witaj swiecie"<<endl;
-	return 0;
+	cout<<"Podj x i y"<<endl;
+	int x, y, suma;
+	cin>>x>>y;
+	suma=x+y;
+	cout<<"suma: "<<suma<<endl;
+
+//2
+if(x>0){
+	cout<<"większe od 0 /n";
+}else{
+	cout<<"mniejsze bądż równe 0";
+}
+
+	return 0; 
 }
