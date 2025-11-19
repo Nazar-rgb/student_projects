b606d73aa12bccc6d5abdcb1ae73d7e893baab35
Nazar-rgb <nazarbudinkevic5@gmail.com>
2025-11-19 11:50:19 +0000

    studentmachine send

--- cpp/cwicenie/main.cpp
+++ cpp/cwicenie/main.cpp
@@ -5,10 +5,18 @@ int main(){
 int liczba;
 cout << "podaj liczbe"<<endl;
 cin >>liczba;
-if(liczba % 2 != 0){
-	cout<<liczba<<" jest pierwszą "<<endl;
-}else{
-	cout<<liczba<<"nie jest pierwsza";}
-
+bool pierwsza = true;
+if(liczba < 1){
+	pierwsza = false;
+}
+for(int i = 2;i< liczba; i++){
+	if(liczba % 2 == 0){
+		pierwsza = false;
+		break;
+		}
+}
+if(pierwsza){
+	cout<<liczba<<" jest pierwsza"<<endl;
+	}else{cout<<liczba<<" nie jest pierwsza"<<endl;}
 return 0;
 }
