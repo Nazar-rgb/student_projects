90b4fe9dd47241c3f6e14dbccac75f0e7741965c
Nazar-rgb <nazarbudinkevic5@gmail.com>
2025-12-03 12:10:51 +0000

    studentmachine send

---
+++ cpp/krzyzyk/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int macierz[3][4] = {
+		{"w","w","w"},
+		{"w","w","w"},
+		{"w","w","w"};
+
+};
+
+cout<<macierz[1][2]<<endl;
+	return 0;
+}
